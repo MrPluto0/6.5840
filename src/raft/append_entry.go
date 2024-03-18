d36c6aa1e000e654ea84aaa34f7632ffd498f4c5
Gypsophlia <710852553@qq.com>
2024-03-18 14:54:26 +0800

    feat: finish lab2A

---
+++ src/raft/append_entry.go
@@ -0,0 +1,45 @@
+package raft
+
+type AppendEntriesArgs struct {
+	Term         int        // leader’s term
+	LeaderId     int        // so follower can redirect clients
+	PrevLogIndex int        // index of log entry immediately preceding new ones
+	PrevLogTerm  int        // term of prevLogIndex entry
+	Entries      []LogEntry // log entries to store (empty for heartbeat; may send more than one for efficiency)
+	LeaderCommit int        // leader’s commitIndex
+}
+
+type AppendEntriesReply struct {
+	Term    int  // currentTerm, for leader to update itself
+	Success bool // true if follower contained entry matching prevLogIndex and prevLogTerm
+}
+
+func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply) {
+	rf.mu.Lock()
+	defer func() {
+		rf.mu.Unlock()
+	}()
+
+	reply.Success = false
+	if args.Term < rf.currentTerm {
+		reply.Term = rf.currentTerm
+		return
+	}
+	if args.Term > rf.currentTerm {
+		rf.updateTerm(args.Term)
+	}
+
+	rf.resetElectionTime()
+	reply.Term = args.Term
+	reply.Success = true
+}
+
+func (rf *Raft) sendAppendEntries(server int, args *AppendEntriesArgs, reply *AppendEntriesReply) bool {
+	ok := rf.peers[server].Call("Raft.AppendEntries", args, reply)
+
+	rf.mu.Lock()
+	DPrintf("[S%d T%d]AppendEntries %v: %v S%d T%d", rf.me, rf.currentTerm, ok, reply.Success, server, reply.Term)
+	rf.mu.Unlock()
+
+	return ok
+}
--- src/raft/util.go
+++ src/raft/util.go
@@ -1,9 +1,11 @@
 package raft
 
-import "log"
+import (
+	"log"
+)
 
 // Debugging
-const Debug = false
+const Debug = true
 
 func DPrintf(format string, a ...interface{}) {
 	if Debug {
